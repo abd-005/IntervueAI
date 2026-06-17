2f67ff53cdadec22314e7e440004d9065f3e5beb
Md Abdul Ahad <abd.me7@gmail.com>
2026-06-17 16:18:27 +0600

    Hero Section Added

---
+++ src/components/Hero.tsx
@@ -0,0 +1,54 @@
+"use client";
+import { motion } from "framer-motion"
+import { ArrowRight } from 'lucide-react'
+
+export const Hero = () => {
+  return (
+    <section className='px-5 py-20'>
+      
+      <div className='max-w-6xl mx-auto'>
+
+        <motion.div
+          initial={{ opacity: 0, y: 30}}
+          animate ={{ opacity: 1, y: 0 }}
+          transition={{ duration: .6 }}
+        >
+
+
+        <h1 className="text-4xl md:text-6xl font-bold tracking-tight">
+          Ace Your IntervueAI
+          <span className="block">
+            With AI Practice
+            </span> 
+        </h1>
+
+        <p className="mt-6 text-muted-foreground text-lg">
+          Generate interview questions,
+          practice answers and get AI feedback.
+        </p>
+
+        <button
+         className="
+            mt-8
+            inline-flex
+            items-center
+            gap-2
+            rounded-full
+            bg-black
+            px-6
+            py-3
+            text-white
+            cursor-pointer
+            "
+            >
+          Start Practice
+
+          <ArrowRight size={18} />
+        </button>
+        
+        </motion.div>
+
+      </div>
+    </section>
+  )
+}
