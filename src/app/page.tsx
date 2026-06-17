4ea46c0015da56df052581da1d5a7d54a8e00e72
Md Abdul Ahad <abd.me7@gmail.com>
2026-06-17 16:28:22 +0600

    Navbar Implemented

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,10 @@
 import { Hero } from "@/components/Hero";
+import { Navbar } from "@/components/Navbar";
 
 export default function Home() {
   return (
     <main className="min-h-screen">
+        <Navbar />
         <Hero />
     </main>
   );
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,5 +1,30 @@
+import { Menu } from "lucide-react"
+
 export const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <header className="sticky top-0 z-50 border-b bg-background/80 text-foreground backdrop-blur-2xl">
+      <nav className="mx-auto flex max-w-6xl items-center justify-between px-5 py-4">
+        <h2 className="text-xl font-bold">
+          IntervueAI
+        </h2>
+
+        {/* Mobile Menu Icon */}
+        <button className="md:hidden">
+          <Menu size={24} />
+        </button>
+
+        {/* Desktop Menu */}
+        <div className="hidden md:flex items-center gap-3">
+          <button className="rounded-lg px-4 py-2">
+            Login
+          </button>
+
+          <button className="rounded-lg px-4 py-2 bg-black text-white">
+            Get Started
+          </button>
+        </div>
+
+      </nav>
+    </header>
   )
 }
